2e7d45b7829ca5b40677f5a93781284399f23336
antonio130795 <antoniojimenezarias8@gmail.com>
2023-10-21 13:47:21 +0200

    install tracer dependency

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # calculadora
 Prueba técnica, servicio API calculadora
+
+install:install-file -Dfile=${project.basedir}/src/main/resources/lib/tracer-1.0.0.jar -DgroupId=io.corp.calculator -DartifactId=tracer -Dversion=1.0.0 -Dpackaging=jar
---
+++ src/main/java/com/prueba/calculadora/service/TracerImpl.java
@@ -0,0 +1,13 @@
+package com.prueba.calculadora.service;
+
+import io.corp.calculator.TracerAPI;
+
+public class TracerImpl implements TracerAPI{
+
+	@Override
+	public <T> void trace(T arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

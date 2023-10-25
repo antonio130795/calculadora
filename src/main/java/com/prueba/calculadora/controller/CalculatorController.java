661e14f7ded16c6efd0356a7c30d778b00a2098e
antonio130795 <antoniojimenezarias8@gmail.com>
2023-10-26 00:02:39 +0200

    implement get avaliableOperations

--- src/main/java/com/prueba/calculadora/controller/CalculatorController.java
+++ src/main/java/com/prueba/calculadora/controller/CalculatorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prueba.calculadora.CalculatorApi;
+import com.prueba.calculadora.model.AvailableOperations;
 import com.prueba.calculadora.model.CalculatorInput;
 import com.prueba.calculadora.model.Result;
 import com.prueba.calculadora.service.CalculatorService;
@@ -24,4 +25,11 @@ public class CalculatorController implements CalculatorApi {
 		
 		return ResponseEntity.ok(calculatorService.performOperation(calculatorInput));
 	}
+
+	@Override
+	public ResponseEntity<AvailableOperations> operations() {
+		
+		return ResponseEntity.ok(calculatorService.getAvaliableOperations());
+	}
+
 }
--- src/main/java/com/prueba/calculadora/service/CalculatorService.java
+++ src/main/java/com/prueba/calculadora/service/CalculatorService.java
@@ -1,24 +1,46 @@
 package com.prueba.calculadora.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.prueba.calculadora.exceptions.ResourceNotFoundException;
+import com.prueba.calculadora.model.AvailableOperations;
 import com.prueba.calculadora.model.CalculatorInput;
 import com.prueba.calculadora.model.Result;
 import com.prueba.calculadora.operations.OperationsI;
 
+import io.corp.calculator.TracerImpl;
+
 @Service
 public class CalculatorService {
+	
 	@Autowired
 	Map<String, OperationsI> operations;
+	
+	
+    @Autowired
+    TracerImpl tracer;
 	
 	public Result performOperation(CalculatorInput calculatorInput) {
         checkBody(calculatorInput);
         return operations.get(calculatorInput.getOperation()).calc(calculatorInput);
     }
+	
+	public AvailableOperations getAvaliableOperations() {
+        return listOfOperations();
+    }
+
+	private AvailableOperations listOfOperations() {
+		List<String> operationsList = new ArrayList<String>();
+		AvailableOperations availableOperations = new AvailableOperations();
+		operationsList.addAll(operations.keySet());
+		availableOperations.setAvailableOperations(operationsList);
+		return availableOperations;
+	}
 
     private void checkBody(CalculatorInput calculatorInput) {
         if (calculatorInput.getFirstOperator() == null || calculatorInput.getSecondOperator() == null || calculatorInput.getOperation() == null) {
